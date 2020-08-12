630e8db4e793aadc1ce55fb43cce0a020f8162b6
thermitegod <thermitethegod@gmail.com>
2020-08-12 13:25:21 -0700

    convert to python

---
+++ set-gpu-powerstate.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# 1.0.0
+# 2020-08-11
+
+# Copyright (C) 2020 Brandon Zorn <brandonzorn@cock.li>
+#
+# This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License version 3
+#    as published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+import argparse
+from pathlib import Path
+
+from utils import utils
+from utils import colors
+
+
+class Powerstate:
+    def __init__(self):
+        self.__card = Path() / '/sys/class/drm/card0/device/power_dpm_force_performance_level'
+        self.__current_state = Path.open(self.__card, 'r').read().rstrip()
+
+    def run(self, args):
+        if args.current:
+            c = colors.Colors()
+            print(f'Current GPU power state is: {c.GRE}{self.__current_state}{c.NC}')
+        if args.set:
+            new_state = args.set
+            if self.__current_state == new_state:
+                print(f'GPU state is already \'{self.__current_state}\'')
+                raise SystemExit
+
+            utils.is_root()
+
+            print(f'Current GPU power state is \'{self.__current_state}\', switching state to \'{new_state}\'')
+            self.__card.write_text(new_state)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--current',
+                        action='store_true',
+                        help='Get current GPU power state')
+    parser.add_argument('-s', '--set',
+                        metavar='STATE',
+                        choices=['auto', 'low', 'high'],
+                        help='Set GPU power state')
+    args = parser.parse_args()
+
+    utils.args_required_else_help()
+
+    run = Powerstate()
+    run.run(args)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        raise SystemExit
