0d48042884e34f8fc74d3fcc38e5ded4f901a103
thermitegod <thermitethegod@gmail.com>
2020-09-19 00:25:47 -0700

    mkadvzip: convert to python

---
+++ mkadvzip.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# 1.0.0
+# 2020-09-19
+
+# Copyright (C) 2020 Brandon Zorn <brandonzorn@cock.li>
+#
+# This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License version 3
+#    as published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+import argparse
+import os
+from pathlib import Path
+
+from utils import utils
+
+
+class Optimize:
+    def __init__(self):
+        self.__verbose = False
+
+        self.__size_start = 0
+        self.__size_end = 0
+
+        self.__cpu = os.cpu_count()
+
+    def get_size(self, start: bool):
+        size = sum(f.stat().st_size for f in Path.cwd().glob('**/*') if f.is_file())
+        if start:
+            self.__size_start = size
+        else:
+            self.__size_end = size
+
+    def main(self):
+        if self.__verbose:
+            self.get_size(start=True)
+
+        utils.run_cmd(f'find . -type f -iname \'*.zip\' -print0 | '
+                      f'nice -19 xargs --max-args=1 --max-procs={self.__cpu} --null '
+                      f'advzip -z -4',
+                      sh_wrap=True)
+
+        if self.__verbose:
+            self.get_size(start=False)
+            print(f'Before     : {self.__size_start}')
+            print(f'After      : {self.__size_end}')
+            print(f'% of orig  : {self.__size_end / self.__size_start}')
+
+    def run(self, args):
+        if args.verbose:
+            self.__verbose = True
+
+        self.main()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose',
+                        action='store_true',
+                        help='Print change in dir size')
+    args = parser.parse_args()
+
+    run = Optimize()
+    run.run(args)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        raise SystemExit
