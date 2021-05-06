122bbae7249543cf82e64f0c88b1abbf0e2e76ab
thermitegod <thermitethegod@gmail.com>
2021-05-06 00:32:47 -0700

    snip.py: create class

--- python/snip.py
+++ python/snip.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# 4.5.0
-# 2020-11-21
+# 5.0.0
+# 2021-05-06
 
-# Copyright (C) 2018,2019,2020 Brandon Zorn <brandonzorn@cock.li>
+# Copyright (C) 2018,2019,2020,2021 Brandon Zorn <brandonzorn@cock.li>
 #
 # This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License version 3
@@ -16,20 +16,39 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-
+import argparse
+import sys
 import time
 from pathlib import Path
+
+from loguru import logger
 
 from python.utils.check_env import CheckEnv
 from python.utils.execute import Execute
 
 
+class Snip:
+    def __init__(self):
+        snip_path = f'{Path.home()}/{int(time.time())}.png'
+
+        if CheckEnv.get_script_name() == 'snip-root':
+            Execute(f'gm import -window root {snip_path}')
+        else:
+            Execute(f'gm import {snip_path}')
+
+
 def main():
-    cmd = 'gm import '
+    parser = argparse.ArgumentParser()
+    debug = parser.add_argument_group('debug')
+    debug.add_argument('-L', '--loglevel',
+                       default='INFO',
+                       metavar='LEVEL',
+                       type=str.upper,
+                       choices=['NONE', 'CRITICAL', 'ERROR', 'WARNING', 'INFO', 'VERBOSE', 'DEBUG', 'TRACE'],
+                       help='Levels: %(choices)s')
+    args = parser.parse_args()
 
-    if CheckEnv.get_script_name() == 'snip-root':
-        cmd += '-window root '
+    logger.remove()
+    logger.add(sys.stdout, level=args.loglevel, colorize=True)
 
-    cmd += f'{Path.home()}/{int(time.time())}.png'
-
-    Execute(cmd)
+    Snip()
