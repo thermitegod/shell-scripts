b1f03695dac7e3adebcd637ee74290418072ba26
thermitegod <thermitethegod@gmail.com>
2021-05-06 00:32:40 -0700

    kernel_microcode_gen.py: create class

--- python/kernel_microcode_gen.py
+++ python/kernel_microcode_gen.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# 1.5.0
-# 2021-04-29
+# 1.6.0
+# 2021-05-06
 
 # Copyright (C) 2020,2021 Brandon Zorn <brandonzorn@cock.li>
 #
@@ -21,10 +21,14 @@
 
 # based on: https://wiki.gentoo.org/wiki/AMD_microcode
 
+import argparse
 import os
 import shutil
+import sys
 from pathlib import Path
 from tempfile import TemporaryDirectory
+
+from loguru import logger
 
 from python.utils.execute import Execute
 from python.utils.root_check import RootCheck
@@ -51,6 +55,19 @@ class Microcode:
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    debug = parser.add_argument_group('debug')
+    debug.add_argument('-L', '--loglevel',
+                       default='INFO',
+                       metavar='LEVEL',
+                       type=str.upper,
+                       choices=['NONE', 'CRITICAL', 'ERROR', 'WARNING', 'INFO', 'VERBOSE', 'DEBUG', 'TRACE'],
+                       help='Levels: %(choices)s')
+    args = parser.parse_args()
+
+    logger.remove()
+    logger.add(sys.stdout, level=args.loglevel, colorize=True)
+
     RootCheck(require_root=True)
 
     Microcode()
