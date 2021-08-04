28b9997147771c421b259354d156d4c9316a064f
thermitegod <thermitethegod@gmail.com>
2021-08-04 02:20:02 -0700

    utils/hash_compare.py: misc changes

--- python/utils/hash_compare.py
+++ python/utils/hash_compare.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# 2.1.0
-# 2020-12-13
+# 3.0.0
+# 2021-08-04
 
-# Copyright (C) 2020 Brandon Zorn <brandonzorn@cock.li>
+# Copyright (C) 2020,2021 Brandon Zorn <brandonzorn@cock.li>
 #
 # This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License version 3
@@ -20,23 +20,21 @@ from pathlib import Path
 
 import xxhash
 
-
 class HashCompare:
     def __init__(self, file1: Path, file2: Path):
         super().__init__()
 
-        self.__hash_results: bool = False
-
-        hash_list: list[str] = []
-        for filename in [file1, file2]:
+        self.__hash_list = [0, 0]
+        for idx, filename in enumerate((file1, file2)):
             hasher = xxhash.xxh3_64()
             with Path.open(filename, 'rb') as f:
                 hasher.update(f.read())
-                hash_list.append(hasher.hexdigest())
-
-        if hash_list[0] == hash_list[1]:
-            self.__hash_results = True
-        self.__hash_results = False
+                self.__hash_list[idx] = hasher.hexdigest()
 
     def results(self):
-        return self.__hash_results
+        if self.__hash_list[0] == self.__hash_list[1]:
+            return True
+        return False
+
+    def hash_list(self):
+        return self.__hash_list
