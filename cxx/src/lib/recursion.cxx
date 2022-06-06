09c922df8f7096dd06466617663acdb88c0537c6
thermitegod <thermitethegod@gmail.com>
2022-06-06 00:10:43 -0700

    cxx: lib/recursion

---
+++ cxx/src/lib/recursion.cxx
@@ -0,0 +1,90 @@
+/**
+ * Copyright (C) 2024 Brandon Zorn <brandonzorn@cock.li>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <filesystem>
+
+#include <span>
+#include <vector>
+
+#include <ztd/ztd.hxx>
+
+#include "lib/recursion.hxx"
+
+recursion::find_files::find_files(const std::filesystem::path& path, const i32 max_depth)
+    : start_path_(path), max_depth_(max_depth)
+{
+    this->recursive_find_files(this->start_path_);
+}
+
+void
+recursion::find_files::recursive_find_files(const std::filesystem::path& path) noexcept
+{
+    this->total_descent_ += 1;
+    this->current_depth_ += 1;
+
+    if (!std::filesystem::is_directory(path))
+    {
+        return;
+    }
+
+    for (const auto& entry : std::filesystem::directory_iterator(path))
+    {
+        if (entry.is_directory())
+        {
+            this->only_dir_list_.push_back(entry.path());
+
+            if (this->current_depth_ == this->max_depth_)
+            {
+                continue;
+            }
+
+            recursive_find_files(entry.path().string());
+            this->current_depth_ -= 1;
+            continue;
+        }
+
+        if (entry.is_regular_file())
+        {
+            this->only_file_list_.push_back(entry.path());
+            continue;
+        }
+    }
+}
+
+const std::span<const std::filesystem::path>
+recursion::find_files::only_files() const noexcept
+{
+    return this->only_file_list_;
+}
+
+const std::span<const std::filesystem::path>
+recursion::find_files::only_dirs() const noexcept
+{
+    return this->only_dir_list_;
+}
+
+const std::span<const std::filesystem::path>
+recursion::find_files::all_entries() const noexcept
+{
+    return this->file_dir_list_;
+}
+
+usize
+recursion::find_files::total_descent() const noexcept
+{
+    return this->total_descent_;
+}
---
+++ cxx/src/lib/recursion.hxx
@@ -0,0 +1,85 @@
+/**
+ * Copyright (C) 2024 Brandon Zorn <brandonzorn@cock.li>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <filesystem>
+
+#include <span>
+#include <vector>
+
+#include <ztd/ztd.hxx>
+
+namespace recursion
+{
+class find_files
+{
+  public:
+    find_files() = delete;
+    find_files(const std::filesystem::path& path, const i32 max_depth = -1);
+
+    /**
+     * @brief only_files
+     *
+     * - Get vector of only files in and below a directory.
+     *
+     * @return vector of file paths
+     */
+    const std::span<const std::filesystem::path> only_files() const noexcept;
+
+    /**
+     * @brief only_dirs
+     *
+     * - Get vector of files and dirs in and below a directory.
+     * All other file types are ignored
+     *
+     * @return vector of dir paths
+     */
+    const std::span<const std::filesystem::path> only_dirs() const noexcept;
+
+    /**
+     * @brief all_entries
+     *
+     * - Get vector of files and dirs in and below a directory.
+     * All other file types are ignored
+     *
+     * @return vector of dir paths
+     */
+    const std::span<const std::filesystem::path> all_entries() const noexcept;
+
+    /**
+     * @brief total_descent
+     *
+     * - Total number of nodes descended
+     *
+     * @return descent count
+     */
+    usize total_descent() const noexcept;
+
+  private:
+    void recursive_find_files(const std::filesystem::path& path) noexcept;
+
+    std::filesystem::path start_path_;
+    i32 max_depth_{0};
+    i32 current_depth_{0};
+    usize total_descent_{0};
+
+    std::vector<std::filesystem::path> only_file_list_;
+    std::vector<std::filesystem::path> only_dir_list_;
+    std::vector<std::filesystem::path> file_dir_list_;
+};
+} // namespace recursion
