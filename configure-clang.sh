a086df423eedf7e29096da3047c88adbc70a3458
thermitegod <43489010+thermitegod@users.noreply.github.com>
2026-09-18 20:48:13 -0700

    gcc/clang configure scripts

---
+++ configure-clang.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+export CC=clang
+export CXX=clang++
+export LD=ld.lld
+# LLVM binutils
+export AR=llvm-ar
+export AS=llvm-as
+export CPP=clang-cpp
+export NM=llvm-nm
+export STRIP=llvm-strip
+export RANLIB=llvm-ranlib
+export OBJCOPY=llvm-objcopy
+export STRINGS=llvm-strings
+export OBJDUMP=llvm-objdump
+export READELF=llvm-readelf
+export ADDR2LINE=llvm-addr2line
+
+meson setup \
+    --buildtype=debugoptimized \
+    --prefix=${HOME}/.local ./build-clang
+
+# debug
+# debugoptimized
+
+# -Db_sanitize=undefined -Db_lundef=false \
+# -Db_sanitize=address -Db_lundef=false \
+# -Db_sanitize=address,undefined -Db_lundef=false \
---
+++ configure-gcc.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+export CC=gcc
+export CXX=g++
+export LD=ld.bfd
+# binutils
+export AR=ar
+export AS=as
+export CPP=cpp
+export NM=nm
+export STRIP=strip
+export RANLIB=ranlib
+export OBJCOPY=objcopy
+export STRINGS=strings
+export OBJDUMP=objdump
+export READELF=readelf
+export ADDR2LINE=addr2line
+
+meson setup \
+    --buildtype=debugoptimized \
+    --prefix=${HOME}/.local ./build-gcc
+
+# debug
+# debugoptimized
+
+# -Db_sanitize=undefined -Db_lundef=false \
+# -Db_sanitize=address -Db_lundef=false \
+# -Db_sanitize=address,undefined -Db_lundef=false \
